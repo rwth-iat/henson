9ac5776e27a0d403177e9e2557b5050e0f515069
Stasik0 <github@stasik.com>
2013-12-02 16:35:17 +0100

    adding FB engineering as plugin

--- js/plugins.js
+++ js/plugins.js
@@ -67,5 +67,52 @@ var plugins = [
         +'" style="border: 0; width: 100%; height: 100%; margin: -1px; padding: 0;"></iframe>';
       $('#'+this.name).append(iFrame);
     }
+  },
+  {
+    name: 'engineering-view',
+    title: 'FB Engineering',
+    author: 'Sten Gruener',
+    activate: {baseClass: 'ov/domain'},
+    foreground: false,
+    refresh: true,
+    destroy: true,
+    checkConditions: function() {
+      var req = new XMLHttpRequest();
+      req.open('HEAD', 'http://'+app.serverConnection.getServerAddress()+':'+app.serverConnection.getServerPort()+'/hmi/', false);
+      try{
+        req.send(null);
+        if(req.status == 200) {
+          return 1;
+        } else {
+          req.open('HEAD', window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/hmi/', false);
+          try {
+            req.send(null);
+            if (req.status == 200) return 2;
+          } catch(e) {
+            // do nothing
+          }
+        }
+      } catch(e) {
+        // do nothing
+      }
+      return false;
+    },
+    run: function(activeElementPath, data) {
+      var sl = this.checkConditions();
+      var iFrame = '<iframe src="'
+        +(sl == 1 ? 'http://' : window.location.protocol+'//')
+        +(sl == 1 ? app.serverConnection.getServerAddress() : window.location.hostname)
+        +':'
+        +(sl == 1 ? app.serverConnection.getServerPort() : window.location.port)
+        +'/hmi/?Host='
+        +(sl == 1 ? app.serverConnection.getServerAddress() : window.location.hostname)
+        +'&Server='
+        +$('#server-name').val()
+		+'&Sheet=/TechUnits/cshmi/engineeringFBSheet'
+        +'&FBReference='
+        +activeElementPath
+        +'" style="border: 0; width: 100%; height: 100%; margin: -1px; padding: 0;"></iframe>';
+      $('#'+this.name).empty().append(iFrame);
+    }
   }
 ];
