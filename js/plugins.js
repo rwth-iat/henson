ef5df12667d1513931f203e48bf9a58e9f5221ac
Holger Jeromin <mailgithub@katur.de>
2013-12-03 14:53:45 +0100

    Ruft jetzt direkt das Template auf, statt das Sheet. Das Template ist vom Pfad her fix. (closes #46)
    Doch einfacher als gedacht.

--- js/plugins.js
+++ js/plugins.js
@@ -84,6 +84,7 @@ var plugins = [
         if(req.status == 200) {
           return 1;
         } else {
+          //fixme: had to check if EngineeringTemplate is loaded, but the target path will change in the near future
           req.open('HEAD', window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/hmi/', false);
           try {
             req.send(null);
@@ -108,7 +109,7 @@ var plugins = [
         +(sl == 1 ? app.serverConnection.getServerAddress() : window.location.hostname)
         +'&Server='
         +$('#server-name').val()
-		+'&Sheet=/TechUnits/cshmi/engineeringFBSheet'
+        +'&Sheet=/TechUnits/cshmi/Templates/Engineering/Domainv1'
         +'&FBReference='
         +activeElementPath
         +'" style="border: 0; width: 100%; height: 100%; margin: -1px; padding: 0;"></iframe>';
